5ab677bdc97fad9114757fda597f240fd8f19ec4
tanakaworld <yutaro.tanaka.world@gmail.com>
2017-01-13 02:09:07 +0900

    Implement add a new user

--- routes/users.js
+++ routes/users.js
@@ -1,15 +1,54 @@
 const express = require('express');
 const router = express.Router();
+const Realm = require('realm');
+
+
+const PostSchema = {
+    name: 'User',
+    properties: {
+        name: 'string',
+        age: 'int',
+        createdAt: 'date'
+    }
+};
+const userRealm = new Realm({
+    path: 'user.realm',
+    schema: [PostSchema]
+});
+
+const getUsers = () => {
+    return userRealm.objects('User').sorted('createdAt', true);
+};
+
 
 /* GET users listing. */
 router.get('/', (req, res, next) => {
-    res.render('users');
+    const users = getUsers();
+    res.render('users', {
+        users
+    });
 });
-router.get('/write', (req, res) => {
-    res.render('users/write');
+router.get('/new', (req, res) => {
+    res.render('users/new');
 });
-router.post('/write', (req, res) => {
-    res.send(req.body);
+router.post('/new', (req, res) => {
+    const name = req.body['name'],
+        age = parseInt(req.body['age']),
+        createdAt = new Date();
+
+    userRealm.write(() => {
+        userRealm.create('User', {
+            name,
+            age,
+            createdAt
+        });
+    });
+
+    const users = getUsers();
+    res.render('users', {
+        notice: 'Success!!',
+        users
+    });
 });
 
 module.exports = router;
